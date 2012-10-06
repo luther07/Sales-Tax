5e7e16d98b31ad1b6ca77063f6efacd78d6eaf9d
Mark Johnson <luther07@gmail.com>
2012-10-06 16:33:22 -0500

    Add condition to Product constructor, throws exception if quantity < 1.

--- src/main/java/Sales-Tax/Product.java
+++ src/main/java/Sales-Tax/Product.java
@@ -1,5 +1,15 @@
 package salestax;
 
 public class Product{
-   
+    public static final double basicSalesTax = 10.0;
+    public static final double importDuty = 5.0;
+    private boolean imported;
+    private boolean exempt;
+    private String name;
+    private double basePrice;
+    private int quantity;
+    
+    public Product(int quantity, boolean imported, String name, double basePrice) {
+        if (quantity < 1) throw new java.lang.IllegalArgumentException();
+    }
 }
