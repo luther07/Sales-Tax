de453d099fbd3f3ff292fb333f1db407a69d6fd6
Mark Johnson <luther07@gmail.com>
2012-10-06 17:41:30 -0500

    Add test to ProductNonExempt constructor, test if exception thrown when price is negative.

--- src/test/java/Sales-Tax/ProductNonExemptTests.java
+++ src/test/java/Sales-Tax/ProductNonExemptTests.java
@@ -8,11 +8,16 @@ public class ProductNonExemptTests {
 
     @Test(expected = java.lang.IllegalArgumentException.class)
     public void ProductNonExempt_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
-        ProductNonExempt zeroQuantity = new ProductNonExempt(0, true, "zeroCokes", 1.00);
+        ProductNonExempt zeroQuantity = new ProductNonExempt(0, true, "zero cokes", 1.00);
     }
 
     @Test(expected = java.lang.IllegalArgumentException.class)
     public void ProductNonExempt_OnEmptyName_ExceptionThrown() throws Exception {
         ProductNonExempt emptyName = new ProductNonExempt(1, true, "", 1.00);
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductNonExempt_OnNegativePrice_ExceptionThrown() throws Exception {
+        ProductNonExempt negativePrice = new ProductNonExempt(1, true, "negative price", -1.00);
     }
 }
