c9880584508d64738f97bd2963a9f1caa08b008f
Mark Johnson <luther07@gmail.com>
2012-10-06 18:34:03 -0500

    Added three failing tests, testing public methods, return values.
    Test methods name(), price(), and quantity, for correct return
    valuse.

--- src/test/java/Sales-Tax/ProductNonExemptTests.java
+++ src/test/java/Sales-Tax/ProductNonExemptTests.java
@@ -3,21 +3,40 @@ package salestax;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class ProductNonExemptTests {
 
-    @Test(expected = java.lang.IllegalArgumentException.class)
+     @Test(expected = java.lang.IllegalArgumentException.class)
     public void ProductNonExempt_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
-        ProductNonExempt zeroQuantity = new ProductNonExempt(0, true, "zero cokes", 1.00);
+        ProductNonExempt zeroQuantity = new ProductNonExempt(0, false, "zero cokes", 1.00);
     }
 
     @Test(expected = java.lang.IllegalArgumentException.class)
     public void ProductNonExempt_OnEmptyName_ExceptionThrown() throws Exception {
-        ProductNonExempt emptyName = new ProductNonExempt(1, true, "", 1.00);
+        ProductNonExempt emptyName = new ProductNonExempt(1, false, "", 1.00);
     }
 
     @Test(expected = java.lang.IllegalArgumentException.class)
     public void ProductNonExempt_OnNegativePrice_ExceptionThrown() throws Exception {
-        ProductNonExempt negativePrice = new ProductNonExempt(1, true, "negative price", -1.00);
+        ProductNonExempt negativePrice = new ProductNonExempt(1, false, "negative price", -1.0);
+	}
+
+    @Test
+    public void ProductNonExempt_WhenNameNotEmpty_ThenReturnsCorrectName() {
+        ProductNonExempt nonEmptyName = new ProductNonExempt(1, false, "non-empty name", 1.0);
+        assertEquals("non-empty name", nonEmptyName.name());
+    }
+
+    @Test
+    public void ProductNonExempt_WhenQuantityGreaterThanZero_ThenReturnsCorrectQuantity() {
+        ProductNonExempt quantityGreaterThanZero = new ProductNonExempt(12345, false, "qty > 0", 1.0);
+        assertEquals(12345, quantityGreaterThanZero.quantity());
+    }
+
+    @Test
+    public void ProductNonExempt_WhenBasePriceGreaterThanZero_ReturnsCorrectValue() {
+        ProductNonExempt quantityGreaterThanZero = new ProductNonExempt(1, false, "price > 0", 1.0);
+        assertEquals(1.1, quantityGreaterThanZero.price(), .01);
     }
 }
