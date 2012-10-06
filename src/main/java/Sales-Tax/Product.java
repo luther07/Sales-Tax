32d9cb75e18c6625e1188eaa482482a63c52bd1d
Mark Johnson <luther07@gmail.com>
2012-10-06 17:12:24 -0500

    Add abstract base class Product, which other classes will extend.
    Using an abstract base class will make it easier to extend, with less work.

---
+++ src/main/java/Sales-Tax/Product.java
@@ -0,0 +1,11 @@
+abstract class Product{
+    private static double importDuty;
+    private boolean imported;
+    private String name;
+    private double basePrice;
+    private int quantity;
+
+    abstract String name();
+    abstract double price();
+    abstract int quantity();
+}
