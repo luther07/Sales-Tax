65f071072ef1d6e0d89b512ae8c615713b5b60f5
Mark Johnson <luther07@gmail.com>
2012-10-06 16:25:54 -0500

    Add build properties file and junit test for quantity < 1.
    Test if exception is thrown when quantity less than 1.

--- src/test/java/Sales-Tax/ProductTests.java
+++ src/test/java/Sales-Tax/ProductTests.java
@@ -10,4 +10,10 @@ public class ProductTests {
    public void testTrue() {
       assertEquals("True", true, true);
    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void Product_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
+        Product zeroQuantity = new Product(0, true, "zeroCokes", 1.00);
+            
+    }
 }
