e9c936a70a1fbfbd79b1626ddaacc7fa9a62208d
Mark Johnson <luther07@gmail.com>
2012-10-06 14:53:58 -0500

    Add first junit test.
    Added dummy junit test, which I will delete after I add first real test.

---
+++ src/test/java/Sales-Tax/CalculatorTests.java
@@ -0,0 +1,13 @@
+package salestax;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTests {
+
+   @Test
+   public void testTrue() {
+      assertEquals("True", true, true);
+   }
+}
