fd1352d30c959fb1a01822f577ad6f2f86be6c5f
Mark Johnson <luther07@gmail.com>
2012-10-06 17:22:53 -0500

    Removed dummy test, because we have a real test now.

--- src/test/java/Sales-Tax/ProductNonExemptTests.java
+++ src/test/java/Sales-Tax/ProductNonExemptTests.java
@@ -6,11 +6,6 @@ import static org.junit.Assert.assertEquals;
 
 public class ProductNonExemptTests {
 
-   @Test
-   public void testTrue() {
-      assertEquals("True", true, true);
-   }
-
     @Test(expected = java.lang.IllegalArgumentException.class)
     public void Product_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
         ProductNonExempt zeroQuantity = new ProductNonExempt(0, true, "zeroCokes", 1.00);
