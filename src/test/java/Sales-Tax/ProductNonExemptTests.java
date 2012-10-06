d4d167065c94b9e56b4394fa3d992268e4c50425
Mark Johnson <luther07@gmail.com>
2012-10-06 17:34:20 -0500

    Add test for empty string name, check if Exception thrown.

--- src/test/java/Sales-Tax/ProductNonExemptTests.java
+++ src/test/java/Sales-Tax/ProductNonExemptTests.java
@@ -7,7 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class ProductNonExemptTests {
 
     @Test(expected = java.lang.IllegalArgumentException.class)
-    public void Product_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
+    public void ProductNonExempt_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
         ProductNonExempt zeroQuantity = new ProductNonExempt(0, true, "zeroCokes", 1.00);
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductNonExempt_OnEmptyName_ExceptionThrown() throws Exception {
+        ProductNonExempt emptyName = new ProductNonExempt(1, true, "", 1.00);
     }
 }
