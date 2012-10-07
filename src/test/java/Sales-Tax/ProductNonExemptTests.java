6f030626d58ed4e958e3fbe2fbe0dcbc6b59d864
Mark Johnson <luther07@gmail.com>
2012-10-07 14:59:51 -0500

    Modified tests for price() and tax().
    Methods price() and tax() should return the same amounts no matter
    what quantity is associated with the product.

--- src/test/java/Sales-Tax/ProductNonExemptTests.java
+++ src/test/java/Sales-Tax/ProductNonExemptTests.java
@@ -34,14 +34,14 @@ public class ProductNonExemptTests {
     }
 
     @Test
-    public void ProductNonExempt_WhenUnitPriceGreaterThanZero_ReturnsCorrectValue() {
-        ProductNonExempt quantityGreaterThanZero = new ProductNonExempt(1, false, "price > 0", 1.0);
+    public void ProductNonExempt_WhenUnitPriceGreaterThanZero_ReturnsCorrectPrice() {
+        ProductNonExempt quantityGreaterThanZero = new ProductNonExempt(999, false, "price > 0", 1.0);
         assertEquals(1.1, quantityGreaterThanZero.price(), 0.01);
     }
 
     @Test
-    public void ProductNonExempt_WhenImportedUnitPriceGreaterThanZero_ReturnsCorrectValue() {
-        ProductNonExempt importedQuantityGreaterThanZero = new ProductNonExempt(1, true, "price > 0", 1.0);
+    public void ProductNonExempt_WhenImportedUnitPriceGreaterThanZero_ReturnsCorrectPrice() {
+        ProductNonExempt importedQuantityGreaterThanZero = new ProductNonExempt(999, true, "price > 0", 1.0);
         assertEquals(1.15, importedQuantityGreaterThanZero.price(), 0.01);
     }
 
@@ -53,13 +53,13 @@ public class ProductNonExemptTests {
 
     @Test
     public void ProductNonExempt_WhenImported_ThenTaxFifteenPercent() {
-        ProductNonExempt importedProductTax = new ProductNonExempt(1, true, "imported", 1.0);
+        ProductNonExempt importedProductTax = new ProductNonExempt(999, true, "imported", 1.0);
         assertEquals(1.0*0.15, importedProductTax.tax(), 0.01);
     }
 
     @Test
 	public void ProductNonExempt_WhenNotImported_ThenTaxTenPercent() {
-        ProductNonExempt notImportedProductTax = new ProductNonExempt(1, false, "not imported", 1.0);
+        ProductNonExempt notImportedProductTax = new ProductNonExempt(999, false, "not imported", 1.0);
         assertEquals(1.0*0.10, notImportedProductTax.tax(), 0.01);
     }
 }
