62bf22a49bed5531b81cbb5d86dd34747101f029
Mark Johnson <luther07@gmail.com>
2012-10-07 17:12:26 -0500

    Add test for ProductExempt name() method.

---
+++ src/main/java/Sales-Tax/ProductExemptTests.java
@@ -0,0 +1,29 @@
+package salestax;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProductExemptTests {
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductExempt_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
+        ProductExempt zeroQuantityExempt = new ProductExempt(0, false, "zero cokes", 1.0);
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductExempt_OnEmptyName_ExceptionThrown() throws Exception {
+        ProductExempt emptyNameExempt = new ProductExempt(1, false, "", 1.0);
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductExempt_OnNegativePrice_ExceptionThrown() throws Exception {
+        ProductExempt negativePriceExempt = new ProductExempt(1, false, "negative price", -1.0);
+    }
+
+    @Test
+    public void ProductExempt_WhenNameNotEmpty_ThenReturnCorrectName() {
+        ProductExempt nonEmptyName = new ProductExempt(1, false, "non-empty name", 1.0);
+        assertEquals("hello world", nonEmptyName.name());
+    }
+}
