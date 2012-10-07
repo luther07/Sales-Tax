a93e5e0d3504a293df2c969f282d858f3c5c6656
Mark Johnson <luther07@gmail.com>
2012-10-07 16:27:18 -0500

    Add tests for illegal ProductExempt constructor.
    Tests fail, because ProductExempt constructor doesn't exist yet.

--- src/main/java/Sales-Tax/ProductExemptTests.java
+++ src/main/java/Sales-Tax/ProductExemptTests.java
@@ -6,4 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class ProductExemptTests {
 
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductExempt_ExceptionThrownIf_QuantityLessThanOne() throws Exception {
+        ProductExempt zeroQuantity = new ProductExempt(0, false, "zero cokes", 1.0);
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductExempt_OnEmptyName_ExceptionThrown() throws Exception {
+        ProductExempt emptyName = new ProductExempt(1, false, "", 1.0);
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException.class)
+    public void ProductExempt_OnNegativePrice_ExceptionThrown() throws Exception {
+        ProductExempt negativePrice = new ProductExempt(1, false, "negative price", -1.0);
+    }
 }
