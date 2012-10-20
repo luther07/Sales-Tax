123417f6ae6a9a1217cafc23e4f16d4a686757a3
Mark Johnson <luther07@gmail.com>
2012-10-20 13:14:09 -0500

    Removed unneeded object from first test, because I'm testing a ReceiptMaker object with zero products, rather than an object with one product.

--- src/test/java/Sales-Tax/ReceiptMakerTests.java
+++ src/test/java/Sales-Tax/ReceiptMakerTests.java
@@ -7,11 +7,7 @@ public class ReceiptMakerTests {
 
     @Test
     public void ReceiptMaker_ReceiptTaxTotal_WhenZeroProducts_EqualsZero() {
-        ProductExempt oneImportedProduct = new ProductExempt(1, true, "imported product", 1.0);
         ReceiptMaker zeroItems = new ReceiptMaker();
         assertEquals(0.0, zeroItems.totalSalesTax(), 0.01);
     }
-
-    @Test
-
 }
