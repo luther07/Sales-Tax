d440fd20959067f5e9d5e13782eb84f5d1d20562
Mark Johnson <luther07@gmail.com>
2012-10-20 13:34:37 -0500

    Add second test, for receiptTotal method, which doesn't exist yet. When there are zero products on a receipt, the receipt total should be 0.

--- src/test/java/Sales-Tax/ReceiptMakerTests.java
+++ src/test/java/Sales-Tax/ReceiptMakerTests.java
@@ -10,4 +10,10 @@ public class ReceiptMakerTests {
         ReceiptMaker zeroItems = new ReceiptMaker();
         assertEquals(0.0, zeroItems.totalSalesTax(), 0.01);
     }
+
+    @Test
+    public void ReceiptMaker_ReceiptTotal_WhenZeroProducts_EqualsZero() {
+        ReceiptMaker zeroItems = new ReceiptMaker();
+        assertEquals(0.0, zeroItems.receiptTotal(), 0.01);
+    }
 }
