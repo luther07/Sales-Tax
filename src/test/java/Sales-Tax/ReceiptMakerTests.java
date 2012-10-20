ae196e80fb1b1df00e94e39cdaf285995e5ee699
Mark Johnson <luther07@gmail.com>
2012-10-20 13:10:52 -0500

    Renamed first method to indicate zero instead of one product is being tested.

--- src/test/java/Sales-Tax/ReceiptMakerTests.java
+++ src/test/java/Sales-Tax/ReceiptMakerTests.java
@@ -6,10 +6,12 @@ import static org.junit.Assert.assertEquals;
 public class ReceiptMakerTests {
 
     @Test
-    public void ReceiptMaker_ReceiptTaxTotal_WhenOneProduct_EqualsProductTaxForProduct() {
+    public void ReceiptMaker_ReceiptTaxTotal_WhenZeroProducts_EqualsZero() {
         ProductExempt oneImportedProduct = new ProductExempt(1, true, "imported product", 1.0);
         ReceiptMaker zeroItems = new ReceiptMaker();
         assertEquals(0.0, zeroItems.totalSalesTax(), 0.01);
     }
+
+    @Test
 
 }
