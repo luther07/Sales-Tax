21360052f5983f9ef3a6799b06f03ba0486be74c
Mark Johnson <luther07@gmail.com>
2012-10-20 13:03:32 -0500

    Removed comments associated with first test.

--- src/test/java/Sales-Tax/ReceiptMakerTests.java
+++ src/test/java/Sales-Tax/ReceiptMakerTests.java
@@ -8,8 +8,6 @@ public class ReceiptMakerTests {
     @Test
     public void ReceiptMaker_ReceiptTaxTotal_WhenOneProduct_EqualsProductTaxForProduct() {
         ProductExempt oneImportedProduct = new ProductExempt(1, true, "imported product", 1.0);
-        // instantiate ReceiptMaker
-        // assertequals ReceiptMaker.totalTax, Product.tax
         ReceiptMaker zeroItems = new ReceiptMaker();
         assertEquals(0.0, zeroItems.totalSalesTax(), 0.01);
     }
