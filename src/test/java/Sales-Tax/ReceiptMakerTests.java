3aa09b7ad6618430753dc2ec997516d68e18ef3a
Mark Johnson <luther07@gmail.com>
2012-10-20 12:54:53 -0500

    First test on the ReceiptMaker class. Writing tests will make the refactoring of ReceiptMaker easier.
    We are testing if the taxes on zero products is 0.0.

---
+++ src/test/java/Sales-Tax/ReceiptMakerTests.java
@@ -0,0 +1,17 @@
+package salestax;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ReceiptMakerTests {
+
+    @Test
+    public void ReceiptMaker_ReceiptTaxTotal_WhenOneProduct_EqualsProductTaxForProduct() {
+        ProductExempt oneImportedProduct = new ProductExempt(1, true, "imported product", 1.0);
+        // instantiate ReceiptMaker
+        // assertequals ReceiptMaker.totalTax, Product.tax
+        ReceiptMaker zeroItems = new ReceiptMaker();
+        assertEquals(0.0, zeroItems.totalSalesTax(), 0.01);
+    }
+
+}
