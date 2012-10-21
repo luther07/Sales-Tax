93eff052fe2d267e2b3f6cc16bde276f2ea1c400
Mark Johnson <luther07@gmail.com>
2012-10-21 16:14:28 -0500

    Added java compiler option -Xlint:deprecated.

--- project/build/sbt.scala
+++ project/build/sbt.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class SalesTax(info: ProjectInfo) extends DefaultProject(info) {
-   override def javaCompileOptions = super.javaCompileOptions ++ javaCompileOptions("-Xlint:unchecked")
+   override def javaCompileOptions = super.javaCompileOptions ++ javaCompileOptions("-Xlint:unchecked") ++ javaCompileOptions("-Xlint:deprecation")
 }
